2d28229ddb2ee87e91c3d2cda9c45ad56160b4bc
InesSousa11 <ines.gomes.sousa@outlook.com>
2025-06-14 02:22:14 +0100

    Added ASTCmpOp that evaluates an expression based on the comparison token it gets. Also added VInt from phase 1. This completes the Rel function from Parser.

---
+++ X++Base/ASTCmpOp.java
@@ -0,0 +1,33 @@
+public class ASTCmpOp implements ASTNode {
+    Token op;
+    ASTNode left, right;
+
+    public ASTCmpOp(Token op, ASTNode left, ASTNode right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+
+    public IValue eval(Environment<IValue> env)	throws
+            InterpreterError {
+        IValue v1 = left.eval(env);
+        IValue v2 = right.eval(env);
+
+        if (!(v1 instanceof VInt) || !(v2 instanceof VInt)) {
+            throw new InterpreterError("Comparison requires integer operands.");
+        }
+
+        int i1 = ((VInt) v1).getval();
+        int i2 = ((VInt) v2).getval();
+
+        return switch (op.kind) {
+            case ParserConstants.EQ -> new VBool(i1 == i2);
+            case ParserConstants.GT -> new VBool(i1 > i2);
+            case ParserConstants.LT -> new VBool(i1 < i2);
+            case ParserConstants.GE -> new VBool(i1 >= i2);
+            case ParserConstants.LE -> new VBool(i1 <= i2);
+            case ParserConstants.NEQ -> new VBool(i1 != i2);
+            default -> throw new InterpreterError("Unknown operator: " + op.image);
+        };
+    }
+}
